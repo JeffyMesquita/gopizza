ea6e62b35e10e37a2ebfff3bcc66ed3e45cc4175
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2022-01-07 16:59:48 -0300

    Estrutura d o Hook de Autenticação
    Autenticação

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import AppLoading from "expo-app-loading";
-import { StatusBar } from 'expo-status-bar'
-import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
-import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
-import { ThemeProvider } from "styled-components/native";
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native';
 
-import theme from "./src/theme";
+import { SignIn } from '@screens/SignIn';
 
-import { SignIn } from "@screens/SignIn";
+import { AuthProvider } from './src/hooks/auth';
+
+import theme from './src/theme';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -21,8 +24,11 @@ export default function App() {
 
 	return (
 		<ThemeProvider theme={theme}>
-      <StatusBar style="light" translucent backgroundColor="transparent" />
-			<SignIn />
+			<StatusBar style="light" translucent backgroundColor="transparent" />
+
+			<AuthProvider>
+				<SignIn />
+			</AuthProvider>
 		</ThemeProvider>
 	);
 }
