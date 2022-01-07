7a2a6ab147120ac553c67daf7720074a6616f7e1
Jeferson Mesquita <je_2742@hotmail.com>
2022-01-07 00:18:47 -0300

    Autenticação
    Componente Button

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { View } from "react-native";
 import AppLoading from "expo-app-loading";
+import { StatusBar } from 'expo-status-bar'
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import { ThemeProvider } from "styled-components/native";
+
+import theme from "./src/theme";
+
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    DMSans_400Regular,
-    DMSerifDisplay_400Regular,
-  });
+	const [fontsLoaded] = useFonts({
+		DMSans_400Regular,
+		DMSerifDisplay_400Regular,
+	});
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
 
-	return <View></View>;
+	return (
+		<ThemeProvider theme={theme}>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+			<SignIn />
+		</ThemeProvider>
+	);
 }
